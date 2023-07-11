41a5e6914134a0232d75c6d74b0d2ba26015fc73
Fanou Razakahasina <fanourazak@gmail.com>
2023-07-11 14:55:23 +0300

    alfsn

--- src/main/java/com/fanou/reseau_social/controller/UserController.java
+++ src/main/java/com/fanou/reseau_social/controller/UserController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
