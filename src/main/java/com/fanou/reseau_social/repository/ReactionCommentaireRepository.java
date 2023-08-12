3dfd35b6f8bae958328f1ed96b3c62499515c010
fanou <fanourazak@gmail.com>
2023-08-12 12:15:32 +0300

    latest dev

--- src/main/java/com/fanou/reseau_social/repository/ReactionCommentaireRepository.java
+++ src/main/java/com/fanou/reseau_social/repository/ReactionCommentaireRepository.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fanou.reseau_social.model.ReactionCommentaire;
-import com.fanou.reseau_social.model.Commentaire;
-
 
 public interface ReactionCommentaireRepository extends JpaRepository<ReactionCommentaire,Long>{
     public Optional<ReactionCommentaire> findByUser_IdUserAndCommentaire_Id(long user_IdUser, long commentaire_Id);
